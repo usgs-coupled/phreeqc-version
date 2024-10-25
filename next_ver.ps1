8dd7fcd5b6fc640aaf9856456803b9e95496b303
Charlton, Scott R. <charlton@usgs.gov>
2024-10-25 11:26:07 -0600

    Fixed next_ver.ps1; bumped version

--- next_ver.ps1
+++ next_ver.ps1
@@ -1,11 +1,14 @@
 $VER = $(Invoke-WebRequest https://raw.githubusercontent.com/usgs-coupled/phreeqc-version/main/phreeqc-version.txt -UseBasicParsing).Content
 $v = $VER -split "\."
 if ([string]::IsNullOrEmpty($v[1])) {
-    $v[2] = 0
-    $v[2] = 0
+  # add v[1]
+  $v += 0
+  # add v[2]
+  $v += 0
 }
 if ([string]::IsNullOrEmpty($v[2])) {
-  $v[2] = 0
+  # add v[2]
+  $v += 0
 }
 else {
   $v[2] = 1 + $v[2]
